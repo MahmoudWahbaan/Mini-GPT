a148c64d3012827bf9929f8f9733c0ff31be62cf
Mahmoud Wahban <MahmoudAhmedWah@Gmail.com>
2026-09-10 03:21:33 +0300

    Softmax

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
 import sys, math
 
-# Scaled dot-product attention scores: Q @ K^T / sqrt(D).
-# Parse 'T <T> D <D>', then T rows of Q, then T rows of K.
-# Emit the T x T score matrix, 4 decimals.
-
-# TODO: implement the dot product and the /sqrt(D) scaling.
-
-line = input().split()
-T = int(line[1])
-D = int(line[-1])
-Query = []
-Key = []
-for i in range(T):
-  line = input().split(',')
-  vec = []
-  for i in range(len(line)):
-    vec.append(int(line[i]))
-  Query.append(vec)
-for i in range(T):
-  line = input().split(',')
-  vec = []
-  for i in range(len(line)):
-    vec.append(int(line[i]))
-  Key.append(vec)
-Attention  = []
-s_d = math.sqrt(D)
-for i in range(T):
-  mat1 = Query[i]
-  res = []
-  for j in range(T):
-    mat2 = Key[j]  
-    calc = 0
-    for dim in range(D):
-      calc = calc+((mat1[dim]*mat2[dim])/(s_d))
-    res.append(round(calc,4))
-  Attention.append(res)
-
-for row in Attention:
-  for i in range(len(row)):
-    if(i==(len(row)-1)):
-      print("{:.4f}".format(row[i]))
-    else:
-      print("{:.4f}".format(row[i]),end = ',')
+# Numerically stable softmax.
+# softmax(x_i) = exp(x_i - max(x)) / sum_j exp(x_j - max(x))
+for raw in sys.stdin:
+    line = raw.rstrip("\n").strip()
+    if not line or not line.startswith("SOFTMAX "):
+        continue
+    x = [float(v) for v in line[8:].split(",")]
+    # TODO: subtract max(x), exponentiate, normalise, print 4-decimal floats.
+    s_x = []
+    sum_exp_j = 0
+    m = max(x)
+    for i in range(len(x)):
+        x[i] =  x[i] - m
+    for j in x:
+        sum_exp_j = sum_exp_j + math.exp(j)
+    for i in range(len(x)):
+        x_i = x[i]
+        exp_xi = math.exp(x_i)
+        s_x.append(round((exp_xi)/(sum_exp_j),4))
+    for i in range(len(s_x)):
+        if(i==(len(s_x)-1)):
+            print("{:.4f}".format(s_x[i]))
+        else:
+            print("{:.4f}".format(s_x[i]),end = ',')   
+        
